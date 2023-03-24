36093dce9917452ca26f544f8e0695b563b3fec3
Hunter Pittman <hunterpittman@protonmail.com>
2023-03-23 22:29:21 -0700

    Update install51.ps1

--- install51.ps1
+++ install51.ps1
@@ -1,11 +1,17 @@
 $osVersion = [System.Environment]::OSVersion.Version
-if (($osVersion.Major -eq 6 -and $osVersion.Minor -eq 1) -or ($osVersion.Major -lt 6)) {
+if (($osVersion.Major -eq 6 -and $osVersion.Minor -eq 1) -or ($osVersion.Major -eq 6 -and $osVersion.Minor -eq 2) -or ($osVersion.Major -lt 6)) {
     $psVersion = Get-ChildItem -Path $env:SystemRoot\System32\WindowsPowerShell\v1.0 -Filter "powershell.exe" | Select-Object -ExpandProperty VersionInfo | Select-Object -ExpandProperty FileVersion
     if ($psVersion -lt "5.1.0.0") {
         Write-Output "Installing PowerShell 5.1..."
 
         # Download the PowerShell 5.1 installer
-        $url = "https://download.microsoft.com/download/3/0/D/30DB904F-E9EB-4C22-9630-A63A84FD7E1D/Win7AndW2K8R2-KB3191566-x64.zip"
+        if ($osVersion.Major -eq 6 -and $osVersion.Minor -eq 1) {
+            $url = "https://download.microsoft.com/download/3/0/D/30DB904F-E9EB-4C22-9630-A63A84FD7E1D/Win7AndW2K8R2-KB3191566-x64.zip"
+        } elseif ($osVersion.Major -eq 6 -and $osVersion.Minor -eq 2) {
+            $url = "https://download.microsoft.com/download/3/0/D/30DB904F-E9EB-4C22-9630-A63A84FD7E1D/W2K12-KB3191566-x64.zip"
+        } else {
+            $url = "https://download.microsoft.com/download/3/0/D/30DB904F-E9EB-4C22-9630-A63A84FD7E1D/W2K12R2-KB3191565-x64.zip"
+        }
         $zipfile = "$env:TEMP\PowerShell5.1.zip"
         $webclient = New-Object System.Net.WebClient
         $webclient.DownloadFile($url, $zipfile)
@@ -26,5 +32,5 @@ if (($osVersion.Major -eq 6 -and $osVersion.Minor -eq 1) -or ($osVersion.Major -
         Write-Output "PowerShell 5.1 is already installed."
     }
 } else {
-    Write-Output "This script is only supported on Windows 7 and higher."
+    Write-Output "This script is only supported on Windows 7, 8, and Server 2012."
 }
